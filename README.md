e1347065bb38fc8614f1349c7fc064a38db41957
pankaj <pankaj.krjha.cse17@itbhu.ac.in>
2021-05-02 01:35:06 +0545

    Added Quality and gitignore

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # pdf_processing
 pip3 install pdf2image
+
 pip3 install PyPDF2
+
 pip3 install img2pdf
 
 rename your pdf file as example.pdf and place in root dir
+
+(for creating all the relevant directories if they don't exist)
+python3 createdirs.py
 
 (get images in images directory)
 python3 getimages.py 
--- getimages.py
+++ getimages.py
@@ -6,6 +6,9 @@ import tempfile
 
 pdf_file="example.pdf"
 dpi=150
+quality=100
+
+
 pdf = PyPDF2.PdfFileReader(pdf_file,"rb")
 p = pdf.getPage(0)
 w_in_user_space_units = p.mediaBox.getWidth()
@@ -15,19 +18,20 @@ h_in_user_space_units = p.mediaBox.getHeight()
 # 1 user space unit is 1/72 inch
 # 1/72 inch ~ 0.352 millimeters
 # pixels = inches * PPI
+# p.mediaBox.getWidth() returns width in user space unit
 
 w = float(p.mediaBox.getWidth())*dpi/72.0
 h = float(p.mediaBox.getHeight())*dpi/72.0
 print(w,h)
 # Store Pdf with convert_from_path function
 images=convert_from_path(pdf_file,dpi=dpi,size=(w,h),fmt="jpeg",jpegopt={
-    "quality": 100,
+    "quality": quality,
     "progressive": True,
     "optimize": True
 })
  
-print(type(images[0]))
 for i in range(len(images)):
     save_path=os.path.join("images",'page'+ str(i+1) +'.jpeg')
     im=images[i]
-    im.save(save_path, 'JPEG',quality=100,dpi=(dpi, dpi))
+    im.save(save_path, 'JPEG',quality=quality,dpi=(dpi, dpi))
+    print("Sucessfuly Saved ",save_path)
